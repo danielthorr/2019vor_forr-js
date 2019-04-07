f5d32262a5734f3a863ad438f79848bb2674f671
Daniel Thor Thorisson <danielthorr@gmail.com>
2019-04-07 22:07:32 +0000

    Finished verkefni 6
    I have yet to finish the .md file so I will do that on the github page

--- Verkefni6/part2 - taggedimages/images.js
+++ Verkefni6/part2 - taggedimages/images.js
@@ -1,13 +1,64 @@
 "use strict";
+
+/************************************************************
+ *            Create a class to easily create images        *
+ ************************************************************/
 class ImgClass
 {
   constructor(name, src, tags = []) {
     this.name = name;
     this.src = src;
     this.tags = tags;
+
+    this.height = 200;
+    this.img = new Image();
+    this.img.src = this.src;
+    this.img.height = this.height;
+
+    this.AddDataAttribute();
+  }
+
+  // This data attribute was never used but I decided to keep it just in case
+  AddDataAttribute(){
+    // We loop through the tags array with an Array.map() function to return
+    // a new array, but we apply join() on it to return it as a string.
+    let arrToString = this.tags.map(tag => {return tag;}).join();
+    this.img.setAttribute("data-tags", arrToString);
   }
 }
 
+/************************************************************
+ *  Fill an array with images, using the image class        *
+ ************************************************************/
 let images = [
-  
-]
+  new ImgClass("Architecture 1", "images/architecture1.jpg", ["Architecture", "Tree", "Modern"]),
+  new ImgClass("Architecture 2", "images/architecture2.jpg", ["Architecture", "Tree", "Nature"]),
+  new ImgClass("Architecture 3", "images/architecture3.jpg", ["Architecture", "Modern", "Highrise"]),
+  new ImgClass("Forest 1", "images/forest1.jpg", ["Nature", "Tree", "Forest"]),
+  new ImgClass("Forest 2", "images/forest2.jpg", ["Nature", "Tree", "Sun"]),
+  new ImgClass("Forest 3", "images/forest3.jpg", ["Nature", "Rock", "Tree"]),
+  new ImgClass("Forest 4", "images/forest4.jpg", ["Nature", "Tree", "Birds Eye"]),
+  new ImgClass("Rock 1", "images/rock1.jpg", ["Tree", "Nature", "Rock"]),
+  new ImgClass("Rock 2", "images/rock2.jpg", ["Rock", "Sea", "Birds Eye"]),
+  new ImgClass("Sea 1", "images/sea1.jpg", ["Sea", "Beach", "Sun"]),
+  new ImgClass("Sea 2", "images/sea2.jpg", ["Sea", "Beach", "Tree"]),
+  new ImgClass("Sea 3", "images/sea3.jpg", ["Sea", "Beach", "Birds Eye"])
+];
+
+/************************************************************
+ *        Work with arrays to get the data we need          *
+ ************************************************************/
+/****** Fill an array with all of the tags we have available ******/
+let tags = [];
+images.forEach((img) => {
+  img.tags.forEach(tag => {
+    tags.push(tag);
+  });
+});
+
+/****** Filter out duplicates from the tags array and sort it  ******/
+// Filter solution found here: 
+// https://stackoverflow.com/questions/1960473/get-all-unique-values-in-a-javascript-array-remove-duplicates
+tags = tags.filter((t, i, self) => self.indexOf(t) === i);
+// Sort tags alphabetically
+tags = tags.sort((a, b) => a>b ? 1 : -1);
--- Verkefni6/part2 - taggedimages/main.js
+++ Verkefni6/part2 - taggedimages/main.js
@@ -1,2 +1,87 @@
 "use strict";
 
+/************************************************************
+ *            Make some functions for convenience           *
+ ************************************************************/
+// Add a function to the element prototype to simplify adding classes
+// it's a little bit pointless but I wanted to see if I could make it work
+Element.prototype.addClass = function(c) {
+  this.classList.add(c);
+}
+
+/****** Create an element, optionally append it to parent and add a class  ******/
+function create(el, parent=undefined, c=undefined) {
+  let tmpEl = document.createElement(el);
+  if (parent !== undefined) {
+    parent.appendChild(tmpEl);
+  }
+  if (c !== undefined) {
+    tmpEl.addClass(c);
+  }
+  return tmpEl;
+}
+
+/************************************************************
+ *          Creating HTML elements for the project          *
+ ************************************************************/
+let mainWrapper = create("div", document.body, "mainWrapper");
+let buttonArea = create("div", mainWrapper, "buttonArea");
+let imageArea = create("div", mainWrapper, "imageArea");
+
+let button = create("div", buttonArea, "tagButton");
+button.innerHTML = "Show all";
+let buttons = [button];
+
+tags.forEach(tag => {
+  let button = create("div", buttonArea, "tagButton");
+  button.innerHTML = tag;
+  buttons.push(button);
+});
+
+images.forEach(image => {
+  imageArea.appendChild(image.img);
+});
+
+/************************************************************
+ *          Make an event listener for the buttons          *
+ *           and create the method for the event            *
+ ************************************************************/
+// I added the event listener to the buttonArea which contains all the buttons
+// I wanted to try out event delegation so that if more buttons were added during
+// runtime, they could also be clicked
+buttonArea.addEventListener("click", FilterImages);
+
+function FilterImages(e) {
+  // For the event delegation to work we need to make sure that we're clicking on
+  // the right element so if it isn't the button we simply return
+  if (e.target.classList[0] !== "tagButton") { return; }
+  
+  // Here we check if an element has the class "active" on it and then remove it
+  buttons.forEach(btn => {
+    if (btn.classList.contains("active")){
+      btn.classList.remove("active");
+      return;
+    }
+  });
+
+  // If the button clicked is "Show all", we simply loop over 
+  // all the images and show them then we return
+  if (e.target.innerHTML === "Show all") {
+    images.forEach(image => { image.img.style.display = "inline"; });
+    e.target.addClass("active");
+    return;
+  }
+
+  images.forEach(img => {
+    // We loop over each image in the array, then we loops over each tag of
+    // the item in the array and filter those who match our selected tag
+    // then we compare the length of the array returned and if it's higher than 0,
+    // that is if any of the tags returned then we display it
+    if (img.tags.filter(tag => (tag === e.target.innerHTML)).length > 0) {
+      img.img.style.display = "inline";
+      e.target.addClass("active");
+    } else {
+      img.img.style.display = "none";
+    }
+  });
+}
